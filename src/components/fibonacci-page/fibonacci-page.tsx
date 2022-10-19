3df1f69d09ad0a133948d5822a05276680b1a525
Avavery <juliya.belka.19@yandex.ru>
2022-10-19 17:00:03 +0300

    string - done

--- src/components/fibonacci-page/fibonacci-page.tsx
+++ src/components/fibonacci-page/fibonacci-page.tsx
@@ -1,15 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { ElementStates } from "../../types/element-states";
 import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from "./fibonacci-page.module.css";
 
 export const FibonacciPage: React.FC = () => {
+  const [inputString, setInputString] = useState<string>("");
+  const [flag, setFlag] = useState<boolean>(false); //флаг для активной кнопки
+  const [massivLetter, setMassivLetter] = useState<number[]>();
+  const [change, setChange] = useState<any>();
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputString(event.target.value);
+  };
+  const massiv = inputString.split(""); //из строки делаем массив
+  const onExpand = () => {
+    setFlag(true);
+    let start = 0;
+    let end = massiv.length - 1;
+    let arr: number[] = [];
+    setChange(ElementStates.Default);
+    for (let i = start; i < end; i++) {
+      //arr = swap(massiv, start, end);
+      start++;
+      end--;
+      setChange(ElementStates.Changing);
+      setMassivLetter(arr);
+    }
+    setChange(ElementStates.Modified);
+    setInputString(""); //очищаем инпут
+    setFlag(false);
+  };
+  const fib = (n: number, memo: Record<number, number> = {}): number => {
+    if (n in memo) {
+      return memo[n];
+    }
+    if (n <= 2) {
+      return 1;
+    }
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+    return memo[n];
+  };
+  console.log(fib(6));
   return (
     <SolutionLayout title="Последовательность Фибоначчи">
       <div className={styles.content}>
-        <Input placeholder="Введите текст" type="text" extraClass={styles.frame} maxLength={19} isLimitText />
-        <Button text="Рассчитать" linkedList="small" />
+        <Input
+          placeholder="Введите текст"
+          type="text"
+          extraClass={styles.frame}
+          maxLength={19}
+          onChange={handleChange}
+          value={inputString}
+          isLimitText
+        />
+        <Button
+          text="Рассчитать"
+          linkedList="small"
+          onClick={onExpand}
+          disabled={inputString !== null ? false : true}
+          isLoader={!flag ? false : true}
+        />
+      </div>
+      <div className={styles.circle}>
+        {massivLetter?.map((letter: number, index: number) => {
+          return <Circle letter={`${letter}`} key={index} state={change} />;
+        })}
       </div>
     </SolutionLayout>
   );
--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,46 +1,58 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { Input } from "../ui/input/input";
 import { Button } from "../ui/button/button";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from "./string.module.css";
 import { Circle } from "../ui/circle/circle";
 import { ElementStates } from "../../types/element-states";
+import { DELAY_IN_MS } from "../../constants/delays";
 
 export const StringComponent: React.FC = () => {
   const [inputString, setInputString] = useState<string>("");
   const [flag, setFlag] = useState<boolean>(false); //флаг для активной кнопки
-  const [massivLetter, setMassivLetter] = useState<string[]>();
-  const [change, setChange] = useState<any>();
+  const [massivLetter, setMassivLetter] = useState<{ letter: string; color: ElementStates }[]>();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputString(event.target.value);
   };
-
-  const swap = (arr: string[], firstIndex: number, secondIndex: number): string[] => {
+  //функция для перестановки символов и замены цвета circle
+  const swap = (
+    arr: { letter: string; color: ElementStates }[],
+    firstIndex: number,
+    secondIndex: number,
+    color: ElementStates
+  ) => {
     const temp = arr[firstIndex];
-    arr[firstIndex] = arr[secondIndex];
-    arr[secondIndex] = temp;
-    return arr;
+    if (firstIndex <= secondIndex) {
+      arr[firstIndex] = arr[secondIndex];
+      arr[secondIndex] = temp;
+      arr[firstIndex].color = color;
+      arr[secondIndex].color = color;
+    }
+    setMassivLetter([...arr]);
+    arr[firstIndex].color = ElementStates.Modified;
+    arr[secondIndex].color = ElementStates.Modified;
   };
-  const massiv = inputString.split(""); //из строки делаем массив
-  const onExpand = () => {
+  //функция при клике на кнопку 'Развернуть'
+  const onExpand = async () => {
     setFlag(true);
-
+    const massiv = inputString.split(""); //из строки делаем массив
+    let arr = massiv.map((item) => {
+      return { letter: item, color: ElementStates.Default };
+    });
     let start = 0;
-    let end = massiv.length - 1;
-    let arr: string[] = [];
-    setChange(ElementStates.Default);
+    let end = arr.length - 1;
 
-    for (let i = start; i < end; i++) {
-      arr = swap(massiv, start, end);
+    setMassivLetter([...arr]);
+    await new Promise((resolve) => setTimeout(resolve, DELAY_IN_MS)); //пауза для отображения начального импута в визуале
+
+    for (let i = start; i <= end; i++) {
+      swap(arr, start, end, ElementStates.Changing);
+      await new Promise((resolve) => setTimeout(resolve, DELAY_IN_MS)); //пауза для визуала переключения цвета circle
       start++;
       end--;
-      setChange(ElementStates.Changing);
-      setMassivLetter(arr);
+      setMassivLetter([...arr]);
     }
-    setChange(ElementStates.Modified);
-
-    setInputString(""); //очищаем инпут
     setFlag(false);
   };
 
@@ -56,12 +68,19 @@ export const StringComponent: React.FC = () => {
           onChange={handleChange}
           value={inputString}
         />
-        <Button text="Развернуть" linkedList="small" onClick={onExpand} disabled={inputString !== `` ? false : true} />
+        <Button
+          text="Развернуть"
+          linkedList="small"
+          onClick={onExpand}
+          disabled={inputString !== `` ? false : true}
+          isLoader={!flag ? false : true}
+        />
       </div>
       <div className={styles.circle}>
-        {massivLetter?.map((letter: string, index: number) => {
-          return <Circle letter={letter} key={index} state={change} />;
-        })}
+        {massivLetter &&
+          massivLetter.map((item, index: number) => {
+            return <Circle letter={item.letter} key={index} state={item.color} />;
+          })}
       </div>
     </SolutionLayout>
   );
