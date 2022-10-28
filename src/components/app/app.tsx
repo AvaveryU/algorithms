3c329daa14762d9b6b1a539fa82ec1a144864504
AvaveryU <juliya.belka.19@yandex.ru>
2022-10-28 23:42:31 +0300

    GH-Pages

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import { FibonacciPage } from "../fibonacci-page/fibonacci-page";
 import { ListPage } from "../list-page/list-page";
 import { MainPage } from "../main-page/main-page";
@@ -13,7 +13,7 @@ import "./app.css";
 function App() {
   return (
     <div className="app">
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route path="/" exact>
             <MainPage />
@@ -37,7 +37,7 @@ function App() {
             <ListPage />
           </Route>
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
