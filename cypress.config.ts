40c519c15bafe438c3a2fb93dc3c718191106784
Avavery <juliya.belka.19@yandex.ru>
2022-11-11 21:54:04 +0300

    начало работы с тестами

---
+++ cypress.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      console.log(config);
+      config.baseUrl = "http://localhost:3000";
+      //implement node event listeners here
+      return config;
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
+    },
+  },
+});
