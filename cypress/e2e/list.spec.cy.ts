441d3e048b893d1d87f1a456c059037fdf26f338
AvaveryU <juliya.belka.19@yandex.ru>
2022-11-17 00:27:28 +0300

    e2e test list page - done

--- cypress/e2e/list.spec.cy.ts
+++ cypress/e2e/list.spec.cy.ts
@@ -1,5 +1,3 @@
-import { ceil } from "cypress/types/lodash";
-
 describe("page list display correctly", () => {
   before(() => {
     cy.visit("/list");
@@ -22,37 +20,136 @@ describe("page list display correctly", () => {
     cy.get('[class*="circle_content"]').should("have.length", 4);
   });
 
-  it("add buttons activation", () => {
-    cy.get('input[placeholder="Введите текст"]').type(`50`);
-    cy.get('button[name="head"]').should("be.enabled").contains("Добавить в head");
-    cy.get('button[name="tail"]').should("be.enabled").contains("Добавить в tail");
-  });
-
+  //добавление элемента в head
   it("add element in head", () => {
-    cy.get('button[name="head"]').contains("Добавить в head").click();
-    //добавление элемента в head
+    cy.get('input[placeholder="Введите текст"]').type(`50`);
+    cy.get('button[name="head"]').should("be.enabled").contains("Добавить в head").click();
     cy.get('[class*="circle_content"]')
       .find("[class*=small]")
+      .first()
       .should("have.css", "border", "4px solid rgb(210, 82, 225)")
       .contains("50");
     cy.get('[class^="circle_circle"]').each(($div, index) => {
       if (index === 0) cy.wrap($div).should("have.css", "border", "4px solid rgb(127, 224, 81)").contains("50");
     });
+    //проверка курсора head
+    cy.get('[class^="circle_content"]').each(($div, index) => {
+      if (index === 0) {
+        cy.wrap($div).first().contains("head");
+      }
+    });
     cy.get('[class*="circle_circle"]').should("have.css", "border", "4px solid rgb(0, 50, 255)");
     cy.get('input[placeholder="Введите текст"]').clear();
   });
 
-  // it("add element in tail", () => {
-  //   cy.get('button[name="tail"]').contains("Добавить в tail").click();
-  //   //добавление элемента в tail
-  //   cy.get('[class*="circle_content"]').find("[class*=small]").contains("50");
-  //   cy.get('[class^="circle_circle"]').each(($div, index) => {
-  //     if (index === 0) cy.wrap($div).should("have.css", "border", "4px solid rgb(0, 50, 255)").contains("50");
-  //   });
-  //   cy.get('input[placeholder="Введите текст"]').clear();
-  // });
-});
+  //добавление элемента в tail
+  it("add element in tail", () => {
+    cy.get('input[placeholder="Введите текст"]').type(`40`);
+    cy.get('button[name="tail"]').should("be.enabled").contains("Добавить в tail").click();
+    cy.wait(500);
+    cy.get('[class*="circle_content"]')
+      .find("[class*=small]")
+      .last()
+      .should("have.css", "border", "4px solid rgb(210, 82, 225)")
+      .contains("40");
+    cy.wait(500);
+    cy.get('[class*="circle_circle"]').each(($div, index) => {
+      if (index === 5) cy.wrap($div).should("have.css", "border", "4px solid rgb(0, 50, 255)").contains("40");
+    });
+    //проверка курсора tail
+    cy.get('[class*="circle_content"]').each(($div, index) => {
+      if (index === 5) {
+        cy.wrap($div).last().contains("tail");
+      }
+    });
+    cy.get('[class*="circle_circle"]').last().should("have.css", "border", "4px solid rgb(0, 50, 255)");
+    cy.get('input[placeholder="Введите текст"]').clear();
+  });
 
-// --default-color: rgb(0, 50, 255);
-//   --changing-color: rgb(210, 82, 225);
-//   --modified-color: rgb(127, 224, 81);
+  //добавление элемента по индексу
+  it("add element by index", () => {
+    cy.get('input[placeholder="Введите текст"]').type(`50`);
+    cy.get('input[placeholder="Введите индекс"]').type(`3`);
+    cy.get('button[name="addElementIndex"]').should("be.enabled").contains("Добавить по индексу").click();
+    cy.wait(500);
+    let start = 0;
+    while (start <= 3) {
+      cy.get('[class*="circle_content"]')
+        .find("[class*=small]")
+        .should("have.css", "border", "4px solid rgb(210, 82, 225)");
+      start++;
+    }
+    cy.get('[class*="circle_content"]').each(($div, index) => {
+      if (index === 4) {
+        cy.wrap($div).find("[class*=circle_circle]").should("have.css", "border", "4px solid rgb(127, 224, 81)");
+      }
+      cy.get("[class*=circle_circle]").should("have.css", "border", "4px solid rgb(0, 50, 255)");
+      cy.get('input[placeholder="Введите текст"]').clear();
+      cy.get('input[placeholder="Введите индекс"]').clear();
+    });
+  });
+
+  //удаление элемента по индексу
+  it("delete element by index", () => {
+    cy.get('input[placeholder="Введите индекс"]').type(`3`);
+    cy.get('button[name="removeElementIndex"]').should("be.enabled").contains("Удалить по индексу").click();
+    let start = 0;
+    while (start <= 3) {
+      cy.get('[class*="circle_content"]')
+        .find("[class*=small]")
+        .should("have.css", "border", "4px solid rgb(210, 82, 225)");
+      cy.get('[class*="circle_circle"]').each(($div, index) => {
+        if (index <= 3) cy.wrap($div).should("have.css", "border", "4px solid rgb(210, 82, 225)");
+      });
+      start++;
+    }
+    cy.wait(500);
+    //проверка курсора tail
+    cy.get('[class*="circle_content"]').each(($div, index) => {
+      if (index === 6) {
+        cy.wrap($div).last().contains("tail");
+      }
+    });
+    cy.get("[class*=circle_circle]").should("have.css", "border", "4px solid rgb(0, 50, 255)");
+    cy.get('input[placeholder="Введите индекс"]').clear();
+  });
+
+  //удаление из head
+  it("delete element from head", () => {
+    cy.get('button[name="headRemoved"]').should("be.enabled").contains("Удалить из head").click();
+    cy.get('[class*="circle_content"]')
+      .find("[class*='circle_small']")
+      .should("have.css", "border", "4px solid rgb(210, 82, 225)")
+      .should("not.have.text", "");
+    //проверка удаления курсора head
+    cy.get('[class*="circle_content"]').each(($div, index) => {
+      if (index === 0) cy.wrap($div).should("not.have.text", "");
+    });
+    cy.wait(500);
+    cy.get("[class*=circle_circle]").should("have.css", "border", "4px solid rgb(0, 50, 255)");
+    //проверка появления курсора head
+    cy.get('[class*="circle_content"]').each(($div, index) => {
+      if (index === 0) cy.wrap($div).first().contains("head");
+    });
+  });
+
+  //удаление из tail
+  it("delete element from tail", () => {
+    cy.get('button[name="tailRemoved"]').should("be.enabled").contains("Удалить из tail").click();
+    cy.get('[class*="circle_content"]')
+      .find("[class*='circle_small']")
+      .should("have.css", "border", "4px solid rgb(210, 82, 225)")
+      .should("not.have.text", "");
+    cy.wait(500);
+    //проверка удаления курсора tail
+    cy.get('[class*="circle_content"]').each(($div, index) => {
+      if (index === 5) cy.wrap($div).should("not.have.text", "");
+    });
+    cy.get("[class*=circle_circle]").should("have.css", "border", "4px solid rgb(0, 50, 255)");
+    cy.wait(500);
+    //проверка появления курсора tail
+    cy.get('[class*="circle_content"]').each(($div, index) => {
+      if (index === 5) cy.wrap($div).contains("tail");
+    });
+  });
+});
